4e44069212153be47fd818bedc2fc6d12c1b4ff2
Jonathon Morgan <jonathon@goodattheinternet.com>
2014-04-10 17:55:33 -0500

    updating data_files location

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='geograpy',
-      version='0.2.1',
+      version='0.2.2',
       description='Extract place names and meta data from text or URLs',
       url='https://github.com/ushahidi/geograpy',
-      download_url ='https://github.com/ushahidi/geograpy/tarball/0.2.1',
+      download_url ='https://github.com/ushahidi/geograpy/tarball/0.2.2',
       author='Jonathon Morgan',
       author_email='jonathon@ushahidi.com',
       license='MIT',
@@ -17,6 +17,6 @@ setup(name='geograpy',
             'pycountry'
       ],
       scripts=['geograpy/bin/geograpy-nltk'],
-      data_files=[('geograpy/data', ['geograpy/data/GeoLite2-City-Locations.csv', 
+      data_files=[('./geograpy/data', ['geograpy/data/GeoLite2-City-Locations.csv', 
             'geograpy/data/ISO3166ErrorDictionary.csv'])],
       zip_safe=False)
