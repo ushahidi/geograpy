7dca2f35ae73c112868dfe9b67a82c1280e548c1
Jonathon Morgan <jonathon@goodattheinternet.com>
2014-04-10 18:01:09 -0500

    incrementing version

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='geograpy',
-      version='0.2.2',
+      version='0.2.3',
       description='Extract place names and meta data from text or URLs',
       url='https://github.com/ushahidi/geograpy',
-      download_url ='https://github.com/ushahidi/geograpy/tarball/0.2.2',
+      download_url ='https://github.com/ushahidi/geograpy/tarball/0.2.3',
       author='Jonathon Morgan',
       author_email='jonathon@ushahidi.com',
       license='MIT',
