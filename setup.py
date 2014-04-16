e91d9fed7d69ca64356e8f9f215200c22f36e9d2
Jonathon Morgan <jonathon@goodattheinternet.com>
2014-04-16 18:20:39 -0500

    incrementing version

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ except (IOError, ImportError):
    long_description = open('README.md').read()
 
 setup(name='geograpy',
-      version='0.2.5',
+      version='0.3.0',
       description='Extract countries, regions and cities from a URL or text',
       long_description=long_description,
       url='https://github.com/ushahidi/geograpy',
-      download_url ='https://github.com/ushahidi/geograpy/tarball/0.2.5',
+      download_url ='https://github.com/ushahidi/geograpy/tarball/0.3.0',
       author='Jonathon Morgan',
       author_email='jonathon@ushahidi.com',
       license='MIT',
